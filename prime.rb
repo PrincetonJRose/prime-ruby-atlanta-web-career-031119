54c16db0db4cb64191f34df18dbeec67c99d709d
Princeton Rose <decoyzpr@gmail.com>
2019-03-02 23:02:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,11 @@
 # Add  code here!
 def prime?(number)
-  
+  if number == 1
+    return false
+  elif number == 2 or number == 3 or number == 5
+    return true
+  elsif number % 2 == 0 or number % 3 == 0 or number % 5 == 0
+    return false
+  else
+    return true
 end
