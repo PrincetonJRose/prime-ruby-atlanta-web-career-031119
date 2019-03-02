99a51c489c1a599804ba8fec339d2a4c37c10987
Princeton Rose <decoyzpr@gmail.com>
2019-03-02 23:21:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,10 @@
 # Add  code here!
 def prime?(number)
   number = number.abs
-  for divide in 6..100
+  if number == 2 or number == 3 or number == 5
+    return true
+  end
+  for divide in 2..100
     if number % divide == 0
       return false
     end
